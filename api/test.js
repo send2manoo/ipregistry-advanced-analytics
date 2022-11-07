6b5eed9879b6b70298b2b6c51928a7149f578fc1
send2manoo <send2manoo@gmail.com>
2022-11-07 18:43:01 +0530

    Added ipregistry-advanced-analytics on NOV 7 16:36 PM

--- api/test.js
+++ api/test.js
@@ -56,7 +56,7 @@ module.exports = async (req, res) => {
         request.onreadystatechange = function() {
             if (request.readyState === 4 && request.status === 200) {
                 jsonData = JSON.parse(request.responseText);
-                console.log('Your country is ' + JSON.stringify(jsonData);
+                console.log('Your country is ' + JSON.stringify(jsonData));
             }
         };
         request.open('GET', 'https://api.ipregistry.co/?key=3noaja8hp0usdbyv', true);
